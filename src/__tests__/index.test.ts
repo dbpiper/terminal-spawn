61b2aefddc68a799ddc88625a223b01577ec4e67
David Piper <david.piper@mailfence.com>
2019-03-15 11:06:39 -0500

    fix: resolve design issue with 2.0.0 being unable to kill process
    Resolve a design issue in 2.0.0 where the process that was spawned could
    not be killed. This happened because we started _only_ returning the
    promise, which meant that things worked fine for processes that
    terminated on their own, but not for processes that never terminated
    on their own. Such as a server that needs to be explicitly killed.
    
    Signed-off-by: David Piper <david.piper@mailfence.com>

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,37 +3,56 @@ import terminalSpawn, { terminalSpawnParallel } from '../index';
 describe('terminal-spawn tests', () => {
   describe('single command tests', () => {
     test('runs command and give exit code of zero', async () => {
-      const subprocess = await terminalSpawn('echo "hello world!"');
+      const subprocess = await terminalSpawn('echo "hello world!"').promise;
       expect(subprocess.status).toBe(0);
+    });
+
+    test('runs command that never ends, is killed and give exit code of zero', async () => {
+      const subprocessSpawn = terminalSpawn(`
+        while true
+        do
+          echo "hello world!"
+          sleep 0.25
+        done
+      `);
+      // wait for 500 ms to pass...
+      const timeToWait = 500;
+      await new Promise((resolve, _reject) =>
+        setTimeout(() => {
+          resolve();
+        }, timeToWait)
+      );
+      subprocessSpawn.process.kill();
+      const subprocess = await subprocessSpawn.promise;
+      expect(subprocess.signal).toEqual('SIGTERM');
     });
 
     test('runs command in other directory and give exit code of zero', async () => {
       const subprocess = await terminalSpawn('pwd', {
         cwd: '/home',
-      });
+      }).promise;
       expect(subprocess.status).toBe(0);
     });
 
     test('runs command and gives non-zero exit code', async () => {
       // the shell doesn't have a "blarg" command
-      const subprocess = await terminalSpawn('blarg "hello world!"');
+      const subprocess = await terminalSpawn('blarg "hello world!"').promise;
       expect(subprocess.status).not.toBe(0);
     });
   });
 
   describe('serial tests', () => {
     test('runs commands serially and give exit code of zero', async () => {
-      const subprocess = await terminalSpawn([
-        'echo "hello "',
-        'echo "world!"',
-      ]);
+      const subprocess = await terminalSpawn(['echo "hello "', 'echo "world!"'])
+        .promise;
       expect(subprocess.status).toBe(0);
     });
   });
 
   describe('parallel tests', () => {
     test('runs single command in parallel and give exit code of zero', async () => {
-      const subprocess = await terminalSpawnParallel('echo "hello world!"');
+      const subprocess = await terminalSpawnParallel('echo "hello world!"')
+        .promise;
       expect(subprocess.status).toBe(0);
     });
 
@@ -41,7 +60,7 @@ describe('terminal-spawn tests', () => {
       const subprocess = await terminalSpawnParallel([
         'echo "hello "',
         'echo "world!"',
-      ]);
+      ]).promise;
       expect(subprocess.status).toBe(0);
     });
   });
--- src/index.ts
+++ src/index.ts
@@ -1,124 +1,41 @@
+import { ChildProcess, SpawnOptions, SpawnSyncReturns } from 'child_process';
 import {
-  ChildProcess,
-  spawn,
-  SpawnOptions,
-  SpawnSyncReturns,
-} from 'child_process';
+  terminalSpawnParallelProcess,
+  terminalSpawnProcess,
+  terminalSpawnPromiseWrapper,
+} from './util';
 
-export type SpawnPromiseReturns = SpawnSyncReturns<Buffer>;
+export type SpawnPromiseReturns = Promise<SpawnSyncReturns<Buffer>>;
 export type Command = string | string[];
-export type TerminalSpawn = (
+
+export interface TerminalSpawnReturns {
+  process: ChildProcess;
+  promise: SpawnPromiseReturns;
+}
+
+const terminalSpawn = (
   command: Command,
   options?: SpawnOptions,
-) => ChildProcess;
-
-/* *****************************************************************************
- * Private Variables
- **************************************************************************** */
-
-const _spawnOptions: SpawnOptions = {
-  stdio: 'inherit',
-  shell: true,
-};
-
-const _spawnWithStringParser = (shellCommand: string) => {
-  const shellCommandArray = shellCommand.split(' ');
+): TerminalSpawnReturns => {
+  const subprocess = terminalSpawnProcess(command, options);
+  const promise = terminalSpawnPromiseWrapper(subprocess);
   return {
-    command: shellCommandArray[0],
-    args: shellCommandArray.slice(1),
+    promise,
+    process: subprocess,
   };
 };
 
-const _terminalSpawnPromiseWrapper = (
-  command: Command,
-  spawnFunction: TerminalSpawn,
-  options?: SpawnOptions,
-) =>
-  new Promise<SpawnPromiseReturns>((resolve, _reject) => {
-    const subprocess = spawnFunction(command, options);
-    let stdin = '';
-    let stdout = '';
-    let stderr = '';
-    let error = {
-      name: '',
-      message: '',
-    };
-
-    if (subprocess.stdin) {
-      subprocess.stdin.on('data', (chunk: Buffer) => {
-        stdin += chunk.toString();
-      });
-    }
-
-    if (subprocess.stdout) {
-      subprocess.stdout.on('data', (chunk: Buffer) => {
-        stdout += chunk.toString();
-      });
-    }
-
-    if (subprocess.stderr) {
-      subprocess.stderr.on('data', (chunk: Buffer) => {
-        stderr += chunk.toString();
-      });
-    }
-
-    subprocess.on('error', (err: Error) => {
-      error = err;
-    });
-
-    subprocess.on('close', (code: number, signal: string) => {
-      resolve({
-        error,
-        signal,
-        pid: process.pid,
-        output: [stdin, stdout, stderr],
-        stdout: Buffer.from(stdout),
-        stderr: Buffer.from(stderr),
-        status: code,
-      });
-    });
-  });
-
-const _terminalSpawnProcess: TerminalSpawn = (
+const terminalSpawnParallel = (
   command: Command,
   options?: SpawnOptions,
-) => {
-  let terminalCommand = '';
-  if (Array.isArray(command)) {
-    terminalCommand = command.join(' && ');
-  } else {
-    terminalCommand = command as string;
-  }
-  const commandObj = _spawnWithStringParser(terminalCommand);
-  const spawnOptions: SpawnOptions = { ..._spawnOptions, ...options };
-  return spawn(commandObj.command, commandObj.args, spawnOptions);
+): TerminalSpawnReturns => {
+  const subprocess = terminalSpawnParallelProcess(command, options);
+  const promise = terminalSpawnPromiseWrapper(subprocess);
+  return {
+    promise,
+    process: subprocess,
+  };
 };
-
-const _terminalSpawnParallelProcess: TerminalSpawn = (
-  command: Command,
-  options?: SpawnOptions,
-) => {
-  let terminalCommand = '';
-  if (Array.isArray(command)) {
-    terminalCommand = command.join(' & ');
-    terminalCommand += '; wait';
-  } else {
-    terminalCommand = command as string;
-  }
-  const commandObj = _spawnWithStringParser(terminalCommand);
-  const spawnOptions: SpawnOptions = { ..._spawnOptions, ...options };
-  return spawn(commandObj.command, commandObj.args, spawnOptions);
-};
-
-/* *****************************************************************************
- * Public Variables
- **************************************************************************** */
-
-const terminalSpawn = (command: Command, options?: SpawnOptions) =>
-  _terminalSpawnPromiseWrapper(command, _terminalSpawnProcess, options);
-
-const terminalSpawnParallel = (command: Command, options?: SpawnOptions) =>
-  _terminalSpawnPromiseWrapper(command, _terminalSpawnParallelProcess, options);
 
 export default terminalSpawn;
 
